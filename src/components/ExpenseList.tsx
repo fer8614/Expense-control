24bb1f1700549d53d2f8ff6e7b6dc72b546a3434
fer8614 <fernando.inversionmf@gmail.com>
2024-06-24 10:06:55 -0700

    adding swipe interaction to expenses to edit and delete

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,6 +1,23 @@
-
+import { useMemo } from "react"
+import { useBudget } from "../hooks/useBudget"
+import ExpenseDetail from "./ExpenseDetail"
 export default function ExpenseList() {
+
+    const { state } = useBudget()
+
+    const isEmpty = useMemo(() => state.expenses.length === 0 , [state.expenses]) 
   return (
-    <div>ExpenseList</div>
+    <div className="mt-10"> 
+        { isEmpty ? <p className="text-2xl text-gray-600 font-bold">No expenses yet</p> : (
+            <>
+                <p className="text-2xl text-gray-600 font-bold my-5">List of expenses</p>
+                { state.expenses.map(expense => (
+                    <ExpenseDetail 
+                        key={expense.id} 
+                        expense={expense} />
+                )) }
+            </>
+        ) }
+    </div>
   )
 }
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -5,3 +5,26 @@ export function formatCurrency( amount: number ) {
         currency: 'USD',
     }).format(amount)
 }
+//To use dates in english
+export function formatDataEnglish( dateString: string ) : string {
+    const dateObj = new Date(dateString)
+    const options : Intl.DateTimeFormatOptions = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return new Intl.DateTimeFormat('en-US', options).format(dateObj)
+}
+
+//To use dates in spanish
+export function formatDataSpanish( dateString: string ) : string {
+    const dateObj = new Date(dateString)
+    const options : Intl.DateTimeFormatOptions = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return new Intl.DateTimeFormat('es-ES', options).format(dateObj)
+}
