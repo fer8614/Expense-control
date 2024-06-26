395f47da1f5251b19995eeba12602c1d848be2b9
fer8614 <fernando.inversionmf@gmail.com>
2024-06-26 12:12:46 -0700

    Show expenses of selected category

--- src/components/FilterByCategory.tsx
+++ src/components/FilterByCategory.tsx
@@ -13,7 +13,7 @@ export default function FilterByCategory() {
     <div className="bg-white shadow-lg rounded-lg p-10">
         <form>
             <div className="flex flex-col md:flex-row md:items-center gap-5">
-                <label htmlFor="category">Filter by category</label>
+                <label htmlFor="category" className="font-bold">Filter by category</label>
                 <select 
                     id="category"
                     className="bg-slate-100 p-3 flex-1 rounded"
